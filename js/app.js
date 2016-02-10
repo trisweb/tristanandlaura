068019f79453ce4d9de3f487a5d89e794a2921f8
Tristan Harward <trisweb@gmail.com>
2016-02-09 21:15:58 -0500

    FastClick for mobile; scaffolds of every page!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+// Setup
+// Initialize Fastclick
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body);
+    console.log("FastClick initialized.");
+  }, false);
+}
 // Main App
 // This will have to be merged back with the stuff at home...
 var app = angular.module('Love', [
@@ -5,14 +13,34 @@ var app = angular.module('Love', [
 ])
 
 // Controllers
+
+// <li><a href="#/">Home</a></li>
+// <li><a href="#/about">Our Story</a></li>
+// <li><a href="#/people">People</a></li>
+// <li><a href="#/events">Events</a></li>
+// <li><a href="#/travel">Travel &amp; Lodging</a></li>
+// <li><a href="#/activities">Things to Do</a></li>
+// <li><a href="#/registry">Registry</a></li>
+// <li><a href="#/photos">Photos</a></li>
+
 .controller('Main', function($scope, $rootScope, $document, $timeout, $location) {
   console.log("~~~~~~~~ <3 <3 <3 ~~~~~~~~");
 })
 .controller('HomeCtrl', function($scope) {
-
 })
 .controller('AboutCtrl', function($scope) {
-
+})
+.controller('PeopleCtrl', function($scope) {
+})
+.controller('EventsCtrl', function($scope) {
+})
+.controller('TravelCtrl', function($scope) {
+})
+.controller('ActivitiesCtrl', function($scope) {
+})
+.controller('RegistryCtrl', function($scope) {
+})
+.controller('PhotosCtrl', function($scope) {
 })
 
 // Routes and Navigation
@@ -26,8 +54,32 @@ var app = angular.module('Love', [
         controller: 'HomeCtrl'
       }).
       when('/about', {
-        templateUrl: 'js/views/aboutus.html',
+        templateUrl: 'js/views/about.html',
         controller: 'AboutCtrl'
+      }).
+      when('/people', {
+        templateUrl: 'js/views/people.html',
+        controller: 'PeopleCtrl'
+      }).
+      when('/events', {
+        templateUrl: 'js/views/events.html',
+        controller: 'EventsCtrl'
+      }).
+      when('/travel', {
+        templateUrl: 'js/views/travel.html',
+        controller: 'TravelCtrl'
+      }).
+      when('/activities', {
+        templateUrl: 'js/views/activities.html',
+        controller: 'ActivitiesCtrl'
+      }).
+      when('/registry', {
+        templateUrl: 'js/views/registry.html',
+        controller: 'RegistryCtrl'
+      }).
+      when('/photos', {
+        templateUrl: 'js/views/photos.html',
+        controller: 'PhotosCtrl'
       })
     ;
   }])
